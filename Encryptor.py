ef321e3202c2c4a9bd6f8f7c7340cbccbb21508e
Eniola0x <ibitomihieniola007@gmail.com>
2024-09-13 12:33:51 +0100

    feat: encryption and decryption skeleton.

---
+++ Encryptor.py
@@ -0,0 +1,8 @@
+class Encryptor:
+    def encrypt(self, text_to_encrypt, secret_key):
+        # encryption process
+        pass
+
+    def decrypt(self, text_to_decrypt, secret_key):
+        # decryption process
+        pass
